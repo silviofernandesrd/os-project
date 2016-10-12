fe3ab14da909bb290d41616f803b58384dc5cae7
Silvio Fernandes <silvio.fernandes@resultadosdigitais.com.br>
2016-10-12 13:56:40 -0300

    init seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+clients = Client.create(
+  [
+    { key: 1, name: 'Cliente 1' },
+    { key: 2, name: 'Cliente 2' }
+  ]
+)
+services = Service.create(
+  [
+    { name: 'Serviço 1', value: 10 },
+    { name: 'Serviço 2', value: 20 }
+  ]
+)
+service_orders = ServiceOrder.create(
+  [
+    {
+      client: clients.find { |i| i.key == 1 },
+      service: services.find { |i| i.name.include?('Serviço 1') }
+    },
+    {
+      client: clients.find { |i| i.key == 2 },
+      service: services.find { |i| i.name.include?('Serviço 2') }
+    }
+  ]
+)
